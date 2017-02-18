2d8d541337ba30d19c928f73da0c2e2e3e22dfef
TheAmazingCodeGuru <ghsu0244@gmail.com>
2017-02-18 18:28:09 +0200

    Added files

---
+++ javascript/functions.js
@@ -0,0 +1,9 @@
+/* == Strip Hover animation trigger ==*/
+$(".strip").hover(
+    function(){
+        $(this).addClass("hover");
+    }, function(){
+        $(this).removeClass("hover");
+    }
+);
+/* == Strip Hover animation ==*/
